64272f5c43e51e87b910eaa0cae87388304a583d
blackx90z <ianmorin23@gmail.com>
2018-06-01 14:46:51 -0700

    understanding ng for

--- day02p2/day02p2/src/app/server/server.component.ts
+++ day02p2/day02p2/src/app/server/server.component.ts
@@ -1,8 +1,27 @@
 import { Component } from "@angular/core";
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+    .online{
+        color: white;
+    }
+    .standard{
+        font-size: 24px;
+    }
+    `]
 })
 export class ServerComponent{
+serverId: number = 10;
+serverStatus: string;
+constructor(){
+    this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+}
 
+getServerStatus(){
+    return this.serverStatus;
+}
+getColor(){
+    return this.serverStatus === 'Online' ? 'green' : 'red';
+}
 }
--- day02p2/day02p2/src/app/servers/servers.component.ts
+++ day02p2/day02p2/src/app/servers/servers.component.ts
@@ -1,16 +1,36 @@
 import {Component, OnInit} from '@angular/core'
 
 @Component({
-    selector: 'app-servers',
+    // selector: 'app-servers',
+    selector'.app-servers';
     templateUrl: './servers.component.html',
     // template:`<app-server></app-server>
     // <app-server></app-server>`,
     styleUrls:['./servers.component.css']
 })
 export class ServersComponent implements OnInit{
-    constructor(){}
+    allowNewServer: boolean = false;
+    serverCreationStatus: string = 'No Server was crerated!';
+    serverName: string;
+serverCreated: boolean = false;
+servers = ['testserver', 'testserver2'];
 
-    ngOnInit(){
+    constructor(){
 
-    }
+        setTimeout(() => {
+            this.allowNewServer = true;
+},2000 );
+ngOnInit(){
+    
+}
+onCreateServer(){
+    this.serverCreated = true;
+this.serverCreationStatus = 'New Server was created! Name is' + this.serverName;
+this.servers.push(this.serverName);
+}
+onUpdadeServerName(event: Event){
+console.log(event);
+this.serverName = (<HTMLInputElement>event.target).value;
+}
+}
 }
